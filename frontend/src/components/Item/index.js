4143df1ed41d54b14933f95597b363cafa098789
John Areola <73708569+Jonnie-Dev@users.noreply.github.com>
2023-01-03 21:54:57 +0000

    #INC-5312: Missing image Fixes #2 image preview: https://ibb.co/ZKrTbvQ

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -42,13 +42,16 @@ class Item extends React.Component {
     const canModify =
       this.props.currentUser &&
       this.props.currentUser.username === this.props.item.seller.username;
+      
+    const imageSrc = this.props.item.image
     return (
       <div className="container page" id="item-container">
         <div className="text-dark">
           <div className="row bg-white p-4">
             <div className="col-6">
               <img
-                src={this.props.item.image}
+                src= {imageSrc ? imageSrc : "../../../placeholder.png"}                
+
                 alt={this.props.item.title}
                 className="item-img"
                 style={{ height: "500px", width: "100%", borderRadius: "6px" }}
